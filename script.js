41e61848f56942b361e10139d272ece48d58695c
Pedro Henrique <amarcilene207@gmail.com>
2025-06-23 22:36:15 -0300

    Testando esse site teste do zero.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Exibe uma saudação com base na hora atual
-function exibirSaudacao() {
-    const saudacao = document.getElementById("saudacao");
-    const hora = new Date().getHours();
-
-    if (hora < 12) {
-        saudacao.textContent = "Bom dia!";
-    } else if (hora < 18) {
-        saudacao.textContent = "Boa tarde!";
-    } else {
-        saudacao.textContent = "Boa noite!";
-    }
+function mostrarMensagem() {
+  alert("Olá! Você clicou no botão!");
 }
-
-// Alerta quando o botão é clicado
-function acaoBotao() {
-    alert("Está tudo certo!");
-}
-
-// Executa a saudação ao carregar a página
-window.onload = exibirSaudacao;
