ecd604812214b1926b88e57982ae6996439613a5
Pedro Henrique <amarcilene207@gmail.com>
2025-06-24 21:48:16 -0300

    Buscando evoluir o site teste

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function mostrarMensagem() {
-  alert("Olá! Você clicou no botão!");
-}
+// Espera o DOM estar carregado (script com defer já garante isso)
+const btn = document.getElementById('alertBtn');
+
+btn.addEventListener('click', () => {
+  alert('Olá! Você clicou no botão!');
+});
