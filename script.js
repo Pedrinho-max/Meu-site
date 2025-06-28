479ad94439f937fa8c96650147af2826e4a61f46
Pedro Henrique <amarcilene207@gmail.com>
2025-06-28 12:01:50 -0300

    Aprimorei algumas coisas no html, css e js.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const btn = document.getElementById('alertBtn');
+<script>
+  const btn = document.getElementById("slideBtn");
 
-btn.addEventListener('click', () => {
-  window.open('https://www.google.com', '_blank');
-});
+  btn.addEventListener("click", () => {
+    btn.classList.add("deslizando");
+
+    // Depois de 300ms (duração da transição), volta ao lugar
+    setTimeout(() => {
+      btn.classList.remove("deslizando");
+    }, 300);
+  });
+</script>
