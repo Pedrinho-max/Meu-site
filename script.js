0a4b7a7020474494a5926ea15cc167ac68d0afb6
Pedro Henrique <amarcilene207@gmail.com>
2025-06-27 22:38:06 -0300

    Falta terminar algumas coisas.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('alertBtn');
 
 btn.addEventListener('click', () => {
-  // Abre nova guia com o site desejado
   window.open('https://www.google.com', '_blank');
 });
